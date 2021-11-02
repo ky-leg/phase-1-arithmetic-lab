08aea846d09f1a13c22d977b065b34bfe11e1021
Kyle Goodwin <kyle.goodwin93@gmail.com>
2021-11-01 21:24:14 -0400

    anotha one

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 1 
+let num2 = 62
 
-// Write your code here
+let multiply = num1 * num2 
+
+
+let random = Math.round((Math.random() * 10))
+
+
+let num3 = 68
+let num4 = 8
+let mod = num3%num4
+
+let num5 = 5
+let num6 = 6
+let num7 = 20
+let max = Math.max(num5, num6, num7)
